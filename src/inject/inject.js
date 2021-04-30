ec318cbd57e14aaebe66dfdad3196b9acb1c0d40
fireph <443370+fireph@users.noreply.github.com>
2021-04-29 18:47:59 -0700

    don't let twitch change quality when tab not focused

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -2,6 +2,7 @@ chrome.extension.sendMessage({}, function(response) {
 	var readyStateCheckInterval = setInterval(function() {
 	if (document.readyState === "complete") {
 		clearInterval(readyStateCheckInterval);
+		Object.defineProperty(document, "hidden", { value : false });
 		setInterval(function() {
 			var elems = document.getElementsByClassName("claimable-bonus__icon");
 			for (elem of elems) {
