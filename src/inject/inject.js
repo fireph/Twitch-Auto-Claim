31d3d98e4b5fb01f3a2c085f16735142151b53e9
fireph <443370+fireph@users.noreply.github.com>
2021-04-29 17:45:50 -0700

    initial v1.0.0

---
+++ src/inject/inject.js
@@ -0,0 +1,13 @@
+chrome.extension.sendMessage({}, function(response) {
+	var readyStateCheckInterval = setInterval(function() {
+	if (document.readyState === "complete") {
+		clearInterval(readyStateCheckInterval);
+		setInterval(function() {
+			var elems = document.getElementsByClassName("claimable-bonus__icon");
+			for (elem of elems) {
+				elem.closest(".tw-button").click();
+			}
+		}, 5000);
+	}
+	}, 10);
+});
