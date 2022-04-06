65998ef796b7d6e4411bab8059fdd3e4ab385468
fireph <443370+fireph@users.noreply.github.com>
2022-04-05 17:02:01 -0700

    upgrade to manifest v3

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,29 +1,45 @@
-chrome.extension.sendMessage({}, function(response) {
-	var readyStateCheckInterval = setInterval(function() {
-	if (document.readyState === "complete") {
-		clearInterval(readyStateCheckInterval);
+chrome.extension.sendMessage({}, function (response) {
+  var readyStateCheckInterval = setInterval(function () {
+    if (document.readyState === "complete") {
+      clearInterval(readyStateCheckInterval);
 
-		// Try to trick the site into thinking it's never hidden
-		Object.defineProperty(document, "hidden", {value: false, writable: false});
-		Object.defineProperty(document, "visibilityState", {value: "visible", writable: false});
-		Object.defineProperty(document, "webkitVisibilityState", {value: "visible", writable: false});
-		document.dispatchEvent(new Event("visibilitychange"));
-		document.hasFocus = function () { return true; };
-		 
-		// visibilitychange events are captured and stopped
-		document.addEventListener("visibilitychange", function(e) {
-			e.stopImmediatePropagation();
-		}, true, true);
+      // Try to trick the site into thinking it's never hidden
+      Object.defineProperty(document, "hidden", {
+        value: false,
+        writable: false,
+      });
+      Object.defineProperty(document, "visibilityState", {
+        value: "visible",
+        writable: false,
+      });
+      Object.defineProperty(document, "webkitVisibilityState", {
+        value: "visible",
+        writable: false,
+      });
+      document.dispatchEvent(new Event("visibilitychange"));
+      document.hasFocus = function () {
+        return true;
+      };
 
-		setInterval(function() {
-			var elems = document.getElementsByClassName("claimable-bonus__icon");
-			for (elem of elems) {
-				var button = elem.closest("button");
-				if (button) {
-					button.click();
-				}
-			}
-		}, 1000);
-	}
-	}, 10);
+      // visibilitychange events are captured and stopped
+      document.addEventListener(
+        "visibilitychange",
+        function (e) {
+          e.stopImmediatePropagation();
+        },
+        true,
+        true
+      );
+
+      setInterval(function () {
+        var elems = document.getElementsByClassName("claimable-bonus__icon");
+        for (elem of elems) {
+          var button = elem.closest("button");
+          if (button) {
+            button.click();
+          }
+        }
+      }, 1000);
+    }
+  }, 10);
 });
